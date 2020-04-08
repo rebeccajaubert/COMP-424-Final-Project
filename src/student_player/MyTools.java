bdf2e5a48684c5650582213acc3b24055d3d7169
rebeccajaubert <rebeccajaubert@users.noreply.github.com>
2020-04-08 21:42:55 +0200

    I need to implement goToNugget with heuristic

--- src/student_player/MyTools.java
+++ src/student_player/MyTools.java
@@ -1,7 +1,127 @@
 package student_player;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import Saboteur.SaboteurBoardState;
+import Saboteur.SaboteurMove;
+import Saboteur.cardClasses.SaboteurBonus;
+import Saboteur.cardClasses.SaboteurCard;
+import Saboteur.cardClasses.SaboteurDestroy;
+import Saboteur.cardClasses.SaboteurDrop;
+import Saboteur.cardClasses.SaboteurMalus;
+import Saboteur.cardClasses.SaboteurMap;
+import Saboteur.cardClasses.SaboteurTile;
+import boardgame.Move;
+
+
+//NB: FOR NOW I FORGET VERIFYLEGIT AND ISLEGAL
 public class MyTools {
+	
+    private static int deckAvail = 41; //55-7-7
+    
+    public static void initPosHiddenC(SaboteurBoardState boardState) {
+    	
+    }
+	
     public static double getSomething() {
         return Math.random();
     }
+    
+    //keep track number cards available --> deduce if soon tie
+    public static void updateCardNumberAvailable(SaboteurBoardState boardState) { 	
+    	 	deckAvail--;deckAvail--; //to account for opponents turn
+    }
+    
+    public static SaboteurMove chooseDrop(ArrayList<SaboteurCard> myCurrentHand,int playerid) { 
+    	SaboteurMove drop = null;
+    	int i = 1;
+    	String[] tilesOkToDiscard = {"1","2","3","4"};
+    	int destroysOrmalus=0;
+    	for (SaboteurCard card : myCurrentHand) {
+    		if(card instanceof SaboteurMap) return new SaboteurMove(new SaboteurDrop(),i,0,playerid);
+    		if(card instanceof SaboteurTile) {
+    			SaboteurTile tile = (SaboteurTile) card;
+    			if(Arrays.asList(tilesOkToDiscard).contains(tile.getIdx()) ) return new SaboteurMove(new SaboteurDrop(),i,0,playerid);
+    		}
+    		if(card instanceof SaboteurDestroy) destroysOrmalus++;
+    		if(card instanceof SaboteurMalus) destroysOrmalus++;
+    		i++;
+    	}
+    	i=0;
+    	if(destroysOrmalus!=0) {
+    		for (SaboteurCard card : myCurrentHand) {
+    			if(card instanceof SaboteurDestroy)  return new SaboteurMove(new SaboteurDrop(),i,0,playerid);
+    			if(card instanceof SaboteurMalus) return new SaboteurMove(new SaboteurDrop(),i,0,playerid);
+    			i++;
+    		}
+    	}
+    	return new SaboteurMove(new SaboteurDrop(),1,0,playerid);
+    }
+    
+    
+    public static SaboteurMove searchGold(ArrayList<SaboteurMove> moves, SaboteurTile[][] boardTiles) {
+    	SaboteurMove noMove = null;
+    	for (SaboteurMove move : moves) {
+			if(move.getCardPlayed() instanceof SaboteurMap) {
+				SaboteurMove mapMove = move;
+				int[] pos = mapMove.getPosPlayed();
+				if(boardTiles[pos[0]][pos[1]].getIdx() != "8") continue; 
+				return mapMove;									//check 1st leftmost then middle
+			}
+    	}
+    	return noMove;
+	}
+    
+    //If malus on us then play bonus card 
+    //or destroy or malus if opponent too close goal
+    //or drop card to get bonus if out of 4 still available
+    public static SaboteurMove counterMalus(ArrayList<SaboteurMove> moves) {
+    	SaboteurMove noGoodMove = null;
+    	int bonuses = 0;int destroys =0; int maluses=0;
+    	SaboteurMove destroyMove = null;
+    	SaboteurMove malusMove= null;
+    	
+    	
+    	for (SaboteurMove move : moves) {
+			if(move.getCardPlayed() instanceof SaboteurBonus) {
+				bonuses++;
+				SaboteurMove bonusSaboteurMove = move;
+				return bonusSaboteurMove;
+			}
+			if(move.getCardPlayed() instanceof SaboteurDestroy && destroyMove==null) {
+				destroys++;
+				destroyMove= move;
+			}
+			if(move.getCardPlayed() instanceof SaboteurMalus && malusMove==null) {
+				maluses++;
+				malusMove = move;
+			}
+		}
+    	
+    	if(destroys!=0) return destroyMove;
+    	else if(maluses!=0) return malusMove;
+    	
+    	return noGoodMove;
+    }
+
+
+    public static SaboteurMove goToNugget(ArrayList<SaboteurMove> moves, int posGoldY) {
+    	SaboteurMove path = null; //goal find a move that is descendent (closer to nugget : {12,y} w/ y=3,5,7)
+    	
+    	
+    	
+    	for (SaboteurMove move : moves) {
+    		
+    		if(!(move.getCardPlayed() instanceof SaboteurTile)) continue;
+    		SaboteurTile tile = (SaboteurTile) move.getCardPlayed();
+    		
+    		int[] pos = move.getPosPlayed();
+    		
+    	}
+    	return path;
+    }
+    
+    
+    
 }
--- src/student_player/StudentPlayer.java
+++ src/student_player/StudentPlayer.java
@@ -3,36 +3,104 @@ package student_player;
 import boardgame.Move;
 
 import Saboteur.SaboteurPlayer;
+import Saboteur.cardClasses.SaboteurCard;
+import Saboteur.cardClasses.SaboteurTile;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import Saboteur.SaboteurBoardState;
+import Saboteur.SaboteurMove;
 
 /** A player file submitted by a student. */
 public class StudentPlayer extends SaboteurPlayer {
+	private boolean goldFound = false;
 
-    /**
-     * You must modify this constructor to return your student number. This is
-     * important, because this is what the code that runs the competition uses to
-     * associate you with your agent. The constructor should do nothing else.
-     */
-    public StudentPlayer() {
-        super("260729564");
-    }
+	/**
+	 * You must modify this constructor to return your student number. This is
+	 * important, because this is what the code that runs the competition uses to
+	 * associate you with your agent. The constructor should do nothing else.
+	 */
+	public StudentPlayer() {
+		super("260729564");
+	}
 
-    /**
-     * This is the primary method that you need to implement. The ``boardState``
-     * object contains the current state of the game, which your agent must use to
-     * make decisions.
-     */
-    public Move chooseMove(SaboteurBoardState boardState) {
-    	System.out.println("TEST");
-        // You probably will make separate functions in MyTools.
-        // For example, maybe you'll need to load some pre-processed best opening
-        // strategies...
-        MyTools.getSomething();
+	/**
+	 * This is the primary method that you need to implement. The ``boardState``
+	 * object contains the current state of the game, which your agent must use to
+	 * make decisions.
+	 */
+	public Move chooseMove(SaboteurBoardState boardState) {
+		ArrayList<SaboteurCard> myCurrentHand =  boardState.getCurrentPlayerCards();
+		ArrayList<SaboteurMove> moves = boardState.getAllLegalMoves();
+		int playerid = boardState.getTurnPlayer();
+		SaboteurMove goodmove = null;
+		//HIDDEN POS ARE  5=originPos . hiddenPos = {{originPos+7,originPos-2},{originPos+7,originPos},{originPos+7,originPos+2}};
+		int posGoldY = 0;
 
-        // Is random the best you can do?
-        Move myMove = boardState.getRandomMove();
+		//check if gold found 
+		SaboteurTile[][] boardTiles =boardState.getHiddenBoard(); //board 14x14 but only     	
+		if(boardTiles[5+7][5-2].getIdx() == "nugget") {goldFound=true; posGoldY = 3;}
+		else if(boardTiles[5+7][5].getIdx()== "nugget" ) { goldFound=true; posGoldY = 5;}
+		else if( boardTiles[5+7][5+2].getIdx()== "nugget") {goldFound=true; posGoldY=7; } //should never get here
+		else if(boardTiles[5+7][5-2].getIdx() != "8" && boardTiles[5+7][5].getIdx()!= "8") { goldFound=true; posGoldY=7; }
 
-        // Return your move to be processed by the server.
-        return myMove;
-    }
+		// moves.forEach(move->System.out.println(move.toPrettyString()));
+
+
+
+		//if malus on us
+		if(boardState.getNbMalus(boardState.getTurnPlayer()) > 0) {
+			SaboteurMove testnull = MyTools.counterMalus(moves);
+			if(testnull!=null) goodmove=testnull;
+		}
+
+
+		//if enemy is too close to goal state then should go here
+
+
+
+
+		//1st find gold
+		//I dont know how to account for map cards played by adversary
+		if(!goldFound) { 
+			SaboteurMove testnull = MyTools.searchGold(moves,boardTiles);
+			if(testnull!=null) {
+				goodmove=testnull;
+			}
+			else {
+				goodmove = MyTools.chooseDrop(myCurrentHand, playerid); // HERE should be GoDown from gotonugget()
+			}
+		}
+		else {
+			//get max x and y ?
+			
+			
+			SaboteurMove path = MyTools.goToNugget(moves, posGoldY);
+			if(path != null) {
+				if(!boardState.isLegal(path)) System.out.println("BUG");
+				goodmove= path;
+			}
+			else { goodmove = MyTools.chooseDrop(myCurrentHand,playerid);
+
+			}
+		}
+
+			// You probably will make separate functions in MyTools.
+			// For example, maybe you'll need to load some pre-processed best opening
+			// strategies...
+			MyTools.getSomething();
+
+			// Is random the best you can do?
+			Move myMove = boardState.getRandomMove();
+
+			// Return your move to be processed by the server.
+			
+		
+		if(goodmove == null) {
+			System.out.println("REAL BAD LUCK");
+			return myMove;
+		}
+		return goodmove;
+	}
 }
